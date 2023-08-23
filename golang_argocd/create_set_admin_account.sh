8b49f2919b5604a851cae7e1220da27e855aa03e
David Hernandez <asbeldavid.hernandez@gmail.com>
2023-08-23 18:02:30 +0200

    Update create and set arogocd admin account script

--- golang_argocd/create_set_admin_account.sh
+++ golang_argocd/create_set_admin_account.sh
@@ -2,23 +2,29 @@
 
 set -euo pipefail
 
+echo create argocd namespace
 kubectl create ns argocd  -o=yaml --dry-run=client | kubectl apply --server-side -f -
 
+echo install argocd
 kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
 
 # wait for argocd pods to be ready
+echo wait for argocd-server pods to be ready
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=300s
 
+echo patch argocd-cm configmap
 kubectl patch configmaps -n argocd argocd-cm --type merge --patch-file argocd-cm.json
 
+echo patch argocd-rbac-cm configmap
 kubectl patch configmap argocd-rbac-cm -n argocd --type merge --patch-file argocd-rbac-cm.json
 
+echo set namespace to argocd
 kubectl config set-context --current --namespace=argocd
 
-kubectl port-forward -n argocd svc/argocd-server -n argocd 8080:443 &
+echo argocd login
+argocd login --core
 
-argocd login 127.0.0.1:8080 --insecure --username=admin --password="$(argocd admin initial-password | head -n 1)"
-
+echo get token for foo account
 echo "------------------------------------------"
 echo ""
 echo "Token"
@@ -26,7 +32,16 @@ argocd account generate-token --account foo --core
 echo ""
 echo "------------------------------------------"
 
-
+echo create guestbook argocd application
 argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
 
+echo sync guestbook application
 argocd app sync argocd/guestbook --assumeYes > /dev/null
+
+
+echo Get argocd initial password
+echo "------------------------------------------"
+echo ""
+argocd admin initial-password | head -n 1
+echo ""
+echo "------------------------------------------"
